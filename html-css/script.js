1b8cc28d488b12c88a40cc54134a6401beffc7a0
Erick <ericksebastianchavezgarcia@gmail.com>
2025-10-17 18:27:07 -0500

    [#002] Trayendo datos de archivo .json

--- html-css/script.js
+++ html-css/script.js
@@ -1,6 +1,6 @@
-const buttonsListingSection = document.querySelector(".jobs-listings");
+const container = document.querySelector(".jobs-listings");
 
-buttonsListingSection.addEventListener("click", (event) => {
+container.addEventListener("click", (event) => {
   const element = event.target;
 
   if (element.classList.contains("button-apply-job")) {
@@ -12,10 +12,10 @@ buttonsListingSection.addEventListener("click", (event) => {
 });
 
 const filter_tech = document.querySelector("#filter-technology");
-const articles = document.querySelectorAll(".job-listing-card");
 const jobs = document.querySelector("#filter-location");
 
 filter_tech.addEventListener("change", () => {
+  const articles = document.querySelectorAll(".job-listing-card");
   articles.forEach((article) => {
     const p = article.querySelector("p").textContent.toLowerCase();
     if (p.includes(filter_tech.value)) {
@@ -27,9 +27,10 @@ filter_tech.addEventListener("change", () => {
 });
 
 jobs.addEventListener("change", () => {
+  const articles = document.querySelectorAll(".job-listing-card");
   articles.forEach((job) => {
     //const l = job.dataset.ubicacion;
-    const l = job.getAttribute("data-ubicacion");
+    const l = job.getAttribute("data-modalidad");
     if (jobs.value === "" || jobs.value === l) {
       job.style.display = "flex";
     } else {
@@ -39,11 +40,26 @@ jobs.addEventListener("change", () => {
 });
 
 // ---> Fetch es asíncrono
-
+// Se está creando una promesa (Promise)
 fetch("./data.json")
   .then((res) => {
     return res.json();
   })
   .then((jobs) => {
-    console.log(jobs);
+    jobs.forEach((job) => {
+      const article = document.createElement("article");
+      article.className = "job-listing-card";
+      article.dataset.modalidad = job.data.modalidad;
+      article.dataset.nivel = job.data.nivel;
+      article.dataset.technology = job.data.technology;
+
+      article.innerHTML = `<div>
+          <h3>${job.titulo}</h3>
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="button-apply-job">Aplicar</button>`;
+
+      container.appendChild(article);
+    });
   });
